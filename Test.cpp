4be19a59dcd9c1ddc867c0f6ee9b89c60bb410e9
Jchang363 <55115625+Jchang363@users.noreply.github.com>
2022-04-21 14:43:45 -0400

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -7,22 +7,45 @@ DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 DigitalOut led3(LED3);
 DigitalOut led4(LED4);
-DigitalIn pb1(p5);
-DigitalIn pb2(p6);
-DigitalIn pb3(p7);
-DigitalIn pb4(p8); 
+DigitalIn pbjl(p5);
+DigitalIn pbjr(p6);
+DigitalIn pb1(p7);
+DigitalIn pb2(p8);
+DigitalIn pb3(p9);
+DigitalIn pb4(p10);  
 AnalogIn AnalogXl(p15);
 AnalogIn AnalogYl(p16);
-AnalogIn AnalogXr(P17);
+AnalogIn AnalogXr(p17);
 AnalogIn AnalogYr(p18); 
 
 
 int main(void)
-{  
+{   
+    pbjl.mode(PullUp);
+    pbjr.mode(PullUp);
+    pb1.mode(PullUp);
+    pb2.mode(PullUp);
+    pb3.mode(PullUp);
+    pb4.mode(PullUp);
     while (1)
     {
-        pc.printf("%f",AnalogXl.read());
-        pc.printf("%f",AnalogYl.read());
-
+       // pc.printf("%f",AnalogXl.read());
+//        pc.printf("%f",AnalogYl.read());
+//        pc.printf("%f",AnalogXr.read());
+//        pc.printf("%f",AnalogYr.read());
+        
+        led1 = pb1;
+        led2 = pb2;
+        led3 = pb3; 
+        led4 = pb4; 
+        
+        if (!pbjl)
+        {
+            pc.printf("Joystick 1");
+        }
+        if (!pbjr)
+        {
+            pc.printf("Joystick 2");
+        }
     }
-}
+ }
